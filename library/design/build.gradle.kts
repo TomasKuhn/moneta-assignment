4cb0b24ea916f766f7f5a02f87660edfa364ddbb
Tomas Kuhn <tomas.kuhn@qinshift.com>
2024-09-25 10:14:31 +0200

    Clear dependencies

--- library/design/build.gradle.kts
+++ library/design/build.gradle.kts
@@ -3,6 +3,4 @@ apply<ComposePlugin>()
 
 dependencies {
     implementation(projects.library.localization)
-    implementation(libs.androidx.material3)
-    implementation(platform(libs.androidx.compose.bom))
 }
